349727c958ecfc70ad14c567b2aec44a62501ea6
Alexander Ossa <aossa@intergrupo.com>
2019-06-10 13:34:32 -0500

    3. Create Spring Boot Project

---
+++ spring boot soap service/src/test/java/com/example/howtodoinjava/spring/boot/soap/service/SpringBootSoapServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.howtodoinjava.spring.boot.soap.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSoapServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
